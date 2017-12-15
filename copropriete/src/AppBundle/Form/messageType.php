918203ed3eb27707f799c40aca118876ebcecf29
Unknown <antoinekeribin@hotmail.fr>
2017-12-15 11:56:42 +0100

    Archivage exclusif à l'admin

--- copropriete/src/AppBundle/Form/messageType.php
+++ copropriete/src/AppBundle/Form/messageType.php
@@ -2,21 +2,36 @@
 
 namespace AppBundle\Form;
 
+use Doctrine\DBAL\Types\TextType;
+use FOS\UserBundle\Controller\SecurityController;
+use SensioLabs\Security\SecurityChecker;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Csrf\TokenStorage\TokenStorageInterface;
 
 class messageType extends AbstractType
 {
     /**
      * {@inheritdoc}
      */
+    private $secu;
+
+    public function __construct(AuthorizationCheckerInterface $authorizationChecker)
+    {
+        $this->secu = $authorizationChecker;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('date');
+            $builder->add('date');
             $builder->add('contenu');
-
-            $builder->add('archive');
+            if($this->secu->isGranted('ROLE_ADMIN')) {
+                $builder->add('archive', CheckboxType::class, array( 'empty_data' => 'No'));
+            }
             $builder->add('user_id');
             $builder->add('discussion_id');
     }
@@ -27,8 +42,7 @@ class messageType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'AppBundle\Entity\message',
-            'role' => ['ROLE_USER']
+            'data_class' => 'AppBundle\Entity\message'
         ));
     }
 
