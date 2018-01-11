e334a59a48c9ab2f1d32c39f96921d2b7251d73b
Unknown <antoinekeribin@hotmail.fr>
2018-01-11 10:50:46 +0100

    Merge de la mort

--- copropriete/src/AppBundle/Form/projetType.php
+++ copropriete/src/AppBundle/Form/projetType.php
@@ -32,14 +32,14 @@ class projetType extends AbstractType
                 'html5' => false,
                 'format' => 'MM-dd-yyyy',
                 // add a class that can be selected in JavaScript
-                'attr' => ['class' => 'js-datepicker'],'placeholder'=>'jj/mm/aaaa'))
+                'attr' => ['class' => 'js-datepicker']))
             ->add('date_fin', DateType::class, array(
                 'widget' => 'single_text',
                 // do not render as type="date", to avoid HTML5 date pickers
                 'html5' => false,
                 'format' => 'MM-dd-yyyy',
                 // add a class that can be selected in JavaScript
-                'attr' => ['class' => 'js-datepicker'], 'placeholder'=>'jj/mm/aaaa'))
+                'attr' => ['class' => 'js-datepicker']))
             ->add('discu_id')
             ->add('pj_id', FileType::class, array('empty_data'=>null, 'required'=>false))
             ->add('utilisateurs');
